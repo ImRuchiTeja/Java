f5f1daa869837b5337b5a1312af364691461609f
ImRuchiTeja <ruchitejaramavath@gmail.com>
2025-02-10 02:58:21 +0530

    Posted Find ASCII value of Character program

---
+++ Find_ASCII_Value_Of_Character.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Find_ASCII_Value_Of_Character {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        String str = in.next();
+        char x = str.charAt(0);
+        findAscii(x);
+    }
+    static void findAscii(int x){
+        System.out.println(x);
+    }
+}
