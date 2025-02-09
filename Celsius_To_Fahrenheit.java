4511a02507626377c540e5c0c7f20c105b0592bf
ImRuchiTeja <ruchitejaramavath@gmail.com>
2025-02-10 03:09:59 +0530

    Posted Cesius to Fahrenheit Program

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Celsius_To_Fahrenheit {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the temperature in degree Celsius");
+        float celsius = in.nextFloat();
+        double fahrenheit = Celsius_to_Fahrenheit(celsius);
+        System.out.printf("%.2f degree Celsius in Fahrenheit is: %.2f degree Fahrenheit", celsius, fahrenheit);
+    }
+    static double Celsius_to_Fahrenheit(float celsius){
+        double fahrenheit = (celsius*1.8)+32;
+        return fahrenheit;
+    }
+}
