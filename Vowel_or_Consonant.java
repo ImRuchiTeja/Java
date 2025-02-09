84b3f7f642feb855c7b8cb407a539b666f0561c4
ImRuchiTeja <ruchitejaramavath@gmail.com>
2025-02-10 03:37:48 +0530

    Posted Check Vowel or Consonant Program

---
+++ Vowel_or_Consonant.java
@@ -0,0 +1,23 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Vowel_or_Consonant {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        if(str.length()>1){
+            System.out.println("Enter a valid character");
+        }else {
+            char x = str.toLowerCase().charAt(0);
+            vowelOrConsonant(x);
+        }
+    }
+    static void vowelOrConsonant(char x) {
+        if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+            System.out.println("It is a Vowel");
+        }else {
+            System.out.println("It is a Consonant");
+        }
+    }
+}
