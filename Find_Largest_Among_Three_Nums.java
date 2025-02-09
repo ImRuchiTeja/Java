2dfe639cf04880444ad80f0803173fb772f5fa0c
ImRuchiTeja <ruchitejaramavath@gmail.com>
2025-02-10 03:45:47 +0530

    Posted Find Largest among three numbers Program

---
+++ Find_Largest_Among_Three_Nums.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class Find_Largest_Among_Three_Nums {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = 4;
+        int max = 0;
+        if (a > b) {
+            max = a;
+        } else if (b > a) {
+            max = b;
+        } else{
+            max = c;
+        }
+        System.out.println(max);
+
+    }
+}
